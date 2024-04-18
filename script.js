aa5c6377cd4ec8d3f1d6a74935cc88cf75bf3297
Takenori <takenori113@gmail.com>
2024-04-18 21:54:58 +0900

    まだ途中です

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 // 非同期処理
 export const func1 = () => {
-  return {};
+  const url = `https://jsonplaceholder.typicode.com/todos/1`
+  return fetch(url).then((res) => {
+    return res.json();
+  })
 };
 
 // 直列で非同期処理
-export const func2 = () => {};
+export const func2 = () => {const url = `https://jsonplaceholder.typicode.com/todos/1`;
+const url2 = `https://jsonplaceholder.typicode.com/todos/2`;
+const url3 = `https://jsonplaceholder.typicode.com/todos/3`;
+const url4 = `https://jsonplaceholder.typicode.com/todos/4`;
+const url5 = `https://jsonplaceholder.typicode.com/todos/5`;
+
+fetch(url)
+  .then(res => res.json())  // 最初のリクエストのレスポンスをJSONに変換
+  .then(data => {
+    console.log(data);       // データを出力
+    return fetch(url2);      // 次のURLへのリクエスト
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    return fetch(url3);
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    return fetch(url4);
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    return fetch(url5);
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+};
 
 // 並列で非同期処理
 export const func3 = () => {
